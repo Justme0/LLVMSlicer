51bc0971a22844b0a9a58d152b078030ae8ffc7c
jiangg <jiangg@mail.ustc.edu.cn>
2015-08-16 20:09:50 +0800

    delete linecount.sh

--- linecount.sh
+++
@@ -1,5 +0,0 @@
-echo -e "Line of Code of Headers (under dir. include/)"
-find ./src -name "*.h" | xargs cat | wc -l
-
-echo -e "Line of Code of Source (under dir. lib/)"
-find ./src -name "*.cpp" | xargs cat | wc -l
